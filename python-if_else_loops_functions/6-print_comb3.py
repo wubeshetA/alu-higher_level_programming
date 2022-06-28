5fb48a43aceb21eda7c87df744c1fcd1bb17cfa8
wubeshetA <wubeshet.a.y@gmail.com>
2022-06-29 01:34:11 +0200

    correct pep8 validation check

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 10):
         if i == 8 and j == 9:
             print("89")
             break
-        print("{}{}, ".format(i,j), end="")
+        print("{}{}, ".format(i, j), end="")
