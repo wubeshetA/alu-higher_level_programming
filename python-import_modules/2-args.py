dfa95b5b42c3468c9177b8819aafad9de51bdf91
wubeshetA <wubeshet.a.y@gmail.com>
2022-06-29 17:28:40 +0200

    add main function

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import sys
-args = sys.argv[1:]
-args_len = len(args)
-if args_len == 0:
-    print("0 arguments.")
-elif args_len == 1:
-    print("1 argument:\n1: {}".format(args[0]))
-else:
-    print("{:d} arguments:".format(args_len))
-    for i in range(args_len):
-        print("{:d}: {}".format(i + 1, args[i]))
+
+
+def main():
+    args = sys.argv[1:]
+    args_len = len(args)
+    if args_len == 0:
+        print("0 arguments.")
+    elif args_len == 1:
+        print("1 argument:\n1: {}".format(args[0]))
+    else:
+        print("{:d} arguments:".format(args_len))
+        for i in range(args_len):
+            print("{:d}: {}".format(i + 1, args[i]))
+
+
+if __name__ == "__main__":
+    main()
