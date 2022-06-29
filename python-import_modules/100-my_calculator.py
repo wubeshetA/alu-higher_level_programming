5266124df29e0d17d2dac84b27c92aab2b231c2a
wubeshetA <wubeshet.a.y@gmail.com>
2022-06-29 23:39:39 +0200

    return back to correct code

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -9,9 +9,9 @@ def main():
     if args_len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if args[1] != '+' and args[1] != '-' and args[1] != '/':
+    if args[1] != '+' and args[1] != '-' and args[1] != '*' and args[1] != '/':
         print("operator: %s" % args[1])
-        print("Unknown operator. Available operators: +, - and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     a = int(args[0])
     b = int(args[2])
@@ -20,6 +20,8 @@ def main():
         result = add(a, b)
     elif operator == '-':
         result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
     elif operator == '/':
         result = div(a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
