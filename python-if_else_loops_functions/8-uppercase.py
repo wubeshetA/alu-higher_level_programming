74e6ba2107e553acf5e6e0d17f4f3664f9c265c8
wubeshetA <wubeshet.a.y@gmail.com>
2022-06-29 02:02:36 +0200

    correct pep validation error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-from cmath import e
-
-
 def uppercase(str):
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 123:
             upper = chr(ord(letter) - 32)
             print("{}".format(upper), end='')
         else:
-            print("{}".format(letter),end='')
+            print("{}".format(letter), end='')
