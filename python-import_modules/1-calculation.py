6b13e225040693570fbe528f11d25201ee36438e
wubeshetA <wubeshet.a.y@gmail.com>
2022-06-29 17:04:42 +0200

    add shebang line

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
 
@@ -7,7 +8,7 @@ def main():
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} * {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 
 if __name__ == "__main__":
