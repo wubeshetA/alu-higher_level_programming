dd3806621cdabc13147e2cb857e266c5145f23f4
wubeshetA <wubeshet.a.y@gmail.com>
2022-06-29 02:24:23 +0200

    correct checker error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 123:
             upper = chr(ord(letter) - 32)
-            print("{}".format(upper), end='')
-        else:
-            print("{}".format(letter), end='')
+            letter = upper
+        print("{}".format(letter), end='')
+    print()
