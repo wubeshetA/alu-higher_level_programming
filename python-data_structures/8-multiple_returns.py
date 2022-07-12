b3700e2d72f6058c07aa206b330f9937d551c577
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-12 20:35:17 +0200

    fix tuple return order

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return None, 0
+        return 0, None
     else:
-        return sentence[0], len(sentence)
+        return len(sentence), sentence[0]
