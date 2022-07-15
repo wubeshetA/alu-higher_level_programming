af2f4f8df0df646090047a75e07a28824b220d48
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-15 20:35:21 +0200

    remove test code

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,3 @@
 def only_diff_elements(set_1, set_2):
     symmetric_diff = set_1.symmetric_difference(set_2)
     return symmetric_diff
-
-
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
