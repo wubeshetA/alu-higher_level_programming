d1c3252c631126081c1cf65f4edb72659fd19ac9
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-15 22:55:46 +0200

    add section to consider None value

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def roman_to_int(s):
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
-    for i in range(len(s)):
-        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+    for i in range(len(roman_string)):
+        if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
+            int_val += (rom_val[roman_string[i]] - 2 *
+                        rom_val[roman_string[i - 1]])
         else:
-            int_val += rom_val[s[i]]
+            int_val += rom_val[roman_string[i]]
     return int_val
