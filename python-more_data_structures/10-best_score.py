ddda4bb2733f5835e35bb0e1d355488f8396c172
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-15 23:06:10 +0200

    fix checker fail

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if a_dictionary is None:
+        return None
+    elif len(a_dictionary) == 0:
         return None
     max_key = max(a_dictionary, key=a_dictionary.get)
     return max_key
