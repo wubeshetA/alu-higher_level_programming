8f4b209cf4786b1ce45f97759965d404530a9862
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-20 16:02:02 +0200

    hot fix

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e))
+    except Exception as e:
+        import sys
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
