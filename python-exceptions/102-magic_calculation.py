326b524fe3830254c4c22ae935919347e7186002
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-20 16:58:08 +0200

    add script

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    elif c > b:
-        return a + b
-    return a*b - c
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = a + b
+            pass
+    return (result)
