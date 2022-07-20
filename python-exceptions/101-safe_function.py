242c832bc9d1d023eeb41ca4a57c26a48399b615
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-20 16:05:57 +0200

    hot fix

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -3,5 +3,6 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        print("Exception: {}".format(e))
+        import sys
+        sys.stderr.write("Exception: {}\n".format(e))
         return None
