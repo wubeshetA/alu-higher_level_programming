d8e9f62070c073293124892b2736a4113ee1acc5
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-20 14:31:43 +0200

    fix checker fail

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    print_count = 0
     try:
         for i in range(x):
             if type(my_list[i]) != int:
                 continue
             else:
                 print("{:d}".format(my_list[i]), end="")
+                print_count += 1
         print()
-        return x
+        return print_count
     except TypeError:
         pass
