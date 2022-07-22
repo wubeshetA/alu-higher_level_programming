ece77c9a88cb55d0b65f41d0372aed497ffd4095
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-22 18:12:02 +0200

    hot fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -75,5 +75,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for i in range(self.position[1]):
+                print()
             for i in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
