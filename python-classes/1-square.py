573d18eff4e150091c77e397897bd972e1c29f6e
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-22 16:29:13 +0200

    fix to meet pep8 valication

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,11 +4,10 @@
 
 class Square:
     """Represent a square."""
-    
 
     def __init__(self, size):
         """Initialize a square.
-        
+
         Args:
             size (int): The size of the square."""
         self.__size = size
