1f2ac73120b30fed597e229c68f0cbd2423cc5ba
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-27 02:16:15 +0200

    fix checker fail

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 more class base
@@ -6,8 +5,6 @@ more class base
 
 
 Rectangle = __import__('9-rectangle').Rectangle
-
-
 """
 Square class
 """
@@ -15,6 +12,7 @@ Square class
 
 class Square(Rectangle):
     """ Square Class """
+
     def __init__(self, size):
         """ size init"""
         self.__size = size
