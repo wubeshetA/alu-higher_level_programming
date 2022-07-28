a9ad9369ab1033917426fcccf62bcc84e9c5caaf
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-28 06:49:39 +0200

    fix checker fail

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,5 +11,5 @@ try:
     items = load_from_json_file("add_item.json")
 except FileNotFoundError:
     items = []
-items.extend([all_list])
+items.extend(all_list)
 save_to_json_file(items, "add_item.json")
