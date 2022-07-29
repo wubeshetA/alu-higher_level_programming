0a9277756043c3010f151e68efec1bc53f72c515
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-29 23:40:12 +0200

    fix checker fail

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""A script to insert a new text after a specifc line of text"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    '''module Search and update
-    '''
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) != -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        f.seek(0)
-        f.write("".join(lines))
+    """Insert text after each line containing a given string in a file.
+    Args:
+        filename (str): file name
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
