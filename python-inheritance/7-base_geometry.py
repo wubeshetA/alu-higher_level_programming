12fe1bde1c088c2ca54a694fe71721e7b17e550a
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-29 23:08:10 +0200

    fix checker fail

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Geometry class"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """represent a geometry"""
+    """Reprsent base geometry."""
 
     def area(self):
-        """raise area not implemented exception"""
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validate integer value"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
