59b7e5bf61d8062b010b5d14d4fa44cf940f9445
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-03 22:37:08 +0200

    fix checker fail

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     import requests
     request = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t type: {}".format(type(request.text)))
-    print("\t content: {}".format(request.text))
+    print("\t- type: {}".format(type(request.text)))
+    print("\t- content: {}".format(request.text))
