a155a7fd76e4d2c0fd11c2f3c4a3be76698f9034
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-03 20:45:31 +0200

    fix checker fail

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        print("Error code: {}".format(e.code))
