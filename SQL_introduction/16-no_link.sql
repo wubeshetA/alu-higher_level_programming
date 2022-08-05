d58b6a7284a1babcbd23e8c2e99383a1f4475aa4
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-05 18:01:04 +0200

    fix error

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all recoreds o the table where name is not null
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
