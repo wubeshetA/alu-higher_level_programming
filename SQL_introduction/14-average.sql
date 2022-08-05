c4dfa89d764f0f312a47fc456fc262a8ab622735
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-05 17:58:10 +0200

    fix checker fail

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- script that displays average score from second_table table
-SELECT AVG(score) FROM second_table AS average;
+SELECT AVG(score) AS average FROM second_table;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- script that lists all recoreds o the table where name is not null
+SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
