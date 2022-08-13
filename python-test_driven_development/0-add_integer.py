df9b5a4324992be5229c67c9e579c540665b43d0
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-13 11:04:06 +0200

    fix checker fail

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,14 +6,12 @@ module with add function
 
 def add_integer(a, b=98):
     """
-    add function
+    Return integer addtion of a and b
+    Rasises:
+        TypeError: If either of a or b is a non-integer and not-float.
     """
-    if not isinstance(a, int) or not isinstance(a, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or not isinstance(b, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a + b
+    return (int(a) + int(b))
