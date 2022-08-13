2aaedf8efec2dffce214dd2c25f6d80cb9526787
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-14 01:28:57 +0200

    fix output error

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,10 @@ def text_indentation(text):
     """prints a text with 2 new lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace(". ", ".\n\n")
-    text = text.replace("? ", "?\n\n")
-    text = text.replace(": ", ":\n\n")
+   
+    text = text.replace(".", ".\n\n")
+    text = text.replace("?", "?\n\n")
+    text = text.replace(":", ":\n\n")
+    text = text.rstrip()
+        
     print(text)
