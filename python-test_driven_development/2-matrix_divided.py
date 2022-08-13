4ce78de30a9f73ee7c8a0112286166a12cbb9e3e
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-13 17:37:42 +0200

    fix checker fail

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,18 +21,11 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if len(row) != len(first_row):
             raise TypeError("Each row of the matrix must have the same size")
-        for col in row:
-            if (isinstance(col, int) is not True and isinstance(col, float)
-                    is not True):
-                raise
-            TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
 
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
-
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6]
-# ]
-# print(matrix_divided(matrix, 3))
-# print(matrix)
