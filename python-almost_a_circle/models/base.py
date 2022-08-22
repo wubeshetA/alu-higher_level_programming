413fb909a0f6beca5fcf4282bb20f4c8b912d83d
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-22 23:15:52 +0200

    create file

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+"""Base class"""
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
