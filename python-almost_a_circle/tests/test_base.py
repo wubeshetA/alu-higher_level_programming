f13f90ae6dd0cff13fa894f46acea67559b45fb6
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-24 12:09:13 +0200

    fix checker fail for tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-""" Unittest for Base Class."""
-
-
 import unittest
 from models.base import Base
 
+"""
+This module contains all unittest for Base class
+"""
+
 
 class TestBase(unittest.TestCase):
-    """Test for Base class."""
-
-    def setUp(self) -> None:
-        """Set up the base class."""
+    """
+    Class of functions to run tests
+    """
+    def setUp(self):
+        """
+        function to redirect stdout
+        """
         pass
 
-    def tearDown(self) -> None:
-        """Tear down the base class."""
+    def tearDown(self):
+        """
+        cleans everything
+        """
         pass
 
     def test_id(self):
-        """Test id"""
+        """
+        Test check for id 
+        """
         Base._Base__nb_objects = 0
         b1 = Base()
         b2 = Base()
-        b3 = Base(98)
-        b4 = Base()
+        b3 = Base()
+        b4 = Base(98)
+        b5 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 98)
-        self.assertEqual(b4.id, 3)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 98)
+        self.assertEqual(b5.id, 4)
