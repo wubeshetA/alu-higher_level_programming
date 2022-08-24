a45d3a6cdf16f50aadc3562820eed5bfc0a04f3f
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-24 06:54:37 +0200

    add create method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -67,3 +67,25 @@ class Base:
             r2 = Square(5)
         r2.update(**dictionary)
         return (r2)
+
+    @classmethod
+    def load_from_file(cls):
+        '''
+            loading dict representing the parameters for
+            and instance and from that creating instances
+        '''
+        file_name = cls.__name__ + ".json"
+
+        try:
+            with open(file_name, encoding="UTF8") as fd:
+                content = cls.from_json_string(fd.read())
+        except Exception:
+            return []
+
+        instances = []
+
+        for instance in content:
+            tmp = cls.create(**instance)
+            instances.append(tmp)
+
+        return instances
