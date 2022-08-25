3719335d14a216ab439549e9ecf55e97ef98451b
wubeshetA <wubeshet.a.y@gmail.com>
2022-08-25 20:44:22 +0200

    add module doc

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""A script that contains State models that used to create a table."""
+"""
+class definition of a State and an instance Base
+"""
 
-
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-host = 'localhost'
-username = argv[1]
-passwd = argv[2]
-port = 3306
-db_name = argv[3]
-engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}"
-                       .format(username, passwd, host, port, db_name))
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-    State class that inherits from Base class.
-    It is a model to create a table in a database.
+    State class:
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, cant be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and cant be null
     """
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
