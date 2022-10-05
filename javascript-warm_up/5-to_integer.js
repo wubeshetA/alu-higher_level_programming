e7e9d71801c9b0d7214ecfbc51749811214cbe21
wubeshetA <w.yimam@alustudent.com>
2022-10-05 14:42:40 +0300

    fix checker fail

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (Number(args[0]) == args[0]) {
-  console.log('My number: ' + Number(args[0]));
-} else {
+if (isNaN(parseInt(args[0]))) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + Number(args[0]));
 }
