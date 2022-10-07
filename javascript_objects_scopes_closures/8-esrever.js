0c7e56b5abfdc862bb23103f9ffc8afabbc9be26
wubeshetA <w.yimam@alustudent.com>
2022-10-07 23:38:32 +0300

    fix logic mistake

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const result = [];
   let i = list.length - 1;
-  for (i; i > 0; i--) {
+  for (i; i >= 0; i--) {
     result.push(list[i]);
   }
   return result;
