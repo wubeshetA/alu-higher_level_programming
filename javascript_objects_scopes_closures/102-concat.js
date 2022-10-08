e49a50cb3901e6d5ebf92c34b6b6ddf3b83585cd
wubeshetA <w.yimam@alustudent.com>
2022-10-08 12:03:05 +0300

    hot fix

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fileA = fs.readFileSync(args[0]);
 const fileB = fs.readFileSync(args[1]);
 const fileC = `${fileA}\n${fileB}`;
-fs.writeFileSync('./fileC', args[2], (err) => {
+fs.writeFileSync(args[2], fileC, (err) => {
   if (err) {
     console.log(err);
   }
