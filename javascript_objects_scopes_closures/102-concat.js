50c47155f8bf35afaf4e3e6ef17a712ecb69dec4
wubeshetA <w.yimam@alustudent.com>
2022-10-08 12:07:46 +0300

    fix checker fail

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 const fs = require('fs');
 const fileA = fs.readFileSync(args[0]);
 const fileB = fs.readFileSync(args[1]);
-const fileC = `${fileA}\n${fileB}`;
+const fileC = `${fileA}${fileB}`;
 fs.writeFileSync(args[2], fileC, (err) => {
   if (err) {
     console.log(err);
