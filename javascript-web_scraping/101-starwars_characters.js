5e6b55362b06acd9512608283d47d09cd798290c
wubeshetA <w.yimam@alustudent.com>
2022-10-09 16:16:12 +0300

    fix semistandard code style

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -4,27 +4,26 @@ const request = require('request');
 
 const filmsUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-function printCharacterName(characters, index) {
-    request(characters[index], (error, response, body) => {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            const name = JSON.parse(body).name;
-            console.log(name);
-            if (index < characters.length - 1) {
-                printCharacterName(characters, index + 1);
-            }
-        }
-    })
-}
-
-request(filmsUrl, (error, response, body) => {
+function printCharacterName (characters, index) {
+  request(characters[index], (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      const parseData = JSON.parse(body);
-      const characters = parseData.characters;
-      printCharacterName(characters, 0)
+      const name = JSON.parse(body).name;
+      console.log(name);
+      if (index < characters.length - 1) {
+        printCharacterName(characters, index + 1);
+      }
     }
   });
+}
+
+request(filmsUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const parseData = JSON.parse(body);
+    const characters = parseData.characters;
+    printCharacterName(characters, 0);
+  }
+});
