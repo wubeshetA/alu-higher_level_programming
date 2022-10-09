a18c7dc0fb98078c67c53cb5e8ebd1ec7964b4e5
wubeshetA <w.yimam@alustudent.com>
2022-10-09 16:14:49 +0300

    add file

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -4,20 +4,27 @@ const request = require('request');
 
 const filmsUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(filmsUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const parseData = JSON.parse(body);
-    const characters = parseData.characters;
-    characters.forEach(charUrl => {
-      request(charUrl, (err, resp, bdy) => {
-        if (err) console.log(err);
-        else {
-          const parseCharData = JSON.parse(bdy);
-          console.log(parseCharData.name);
+function printCharacterName(characters, index) {
+    request(characters[index], (error, response, body) => {
+        if (error) {
+            console.log(error);
         }
-      });
-    });
-  }
-});
+        else {
+            const name = JSON.parse(body).name;
+            console.log(name);
+            if (index < characters.length - 1) {
+                printCharacterName(characters, index + 1);
+            }
+        }
+    })
+}
+
+request(filmsUrl, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const parseData = JSON.parse(body);
+      const characters = parseData.characters;
+      printCharacterName(characters, 0)
+    }
+  });
