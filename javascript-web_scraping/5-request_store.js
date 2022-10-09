bac9a8a6f1d2b2209788c86da78f6d150d2fde8b
wubeshetA <w.yimam@alustudent.com>
2022-10-09 14:06:48 +0300

    meet semistandard code style

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,12 +3,11 @@ const request = require('request');
 const fs = require('fs');
 const args = process.argv.slice(2);
 request(args[0], (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        fs.writeFile(args[1], body, 'utf8', (error) => {
-            if (error) console.log(error);
-            else (console.log("file written successfully"));
-        });
-    }
-})
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(args[1], body, 'utf8', (error) => {
+      if (error) console.log(error);
+    });
+  }
+});
