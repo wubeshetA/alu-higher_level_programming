3999df56a16ab1961f426569b7f3ce0663d655b6
wubeshetA <w.yimam@alustudent.com>
2022-10-09 14:04:51 +0300

    add file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const args = process.argv.slice(2);
+request(args[0], (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        fs.writeFile(args[1], body, 'utf8', (error) => {
+            if (error) console.log(error);
+            else (console.log("file written successfully"));
+        });
+    }
+})
